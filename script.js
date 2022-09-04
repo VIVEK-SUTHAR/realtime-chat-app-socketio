583d77c6713073373e638249216fb7f68486ef1c
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-09-04 12:50:02 +0530

    Added

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 const socket = io("http://localhost:3000", {
   transports: ["websocket", "polling", "flashsocket"],
 });
+
+const uname = prompt("Enter your Name:");
 const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
-socket.on("chat-message", (data) => {
-  appendMessage(data)
+
+socket.on("new-user", (data) => {
+  appendMessage(data);
 });
+
+socket.on("chat-message", (data) => {
+  appendMessage(data);
+});
+socket.on("user-connected", (data) => {
+  userJoined(`${data} joined`);
+});
+socket.emit("new-user", uname);
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -14,8 +25,17 @@ messageForm.addEventListener("submit", (e) => {
   socket.emit("send-chat-message", message);
   messageInput.value = "";
 });
-function appendMessage(message) {
-    const messageDiv = document.createElement("div");
-    messageDiv.innerHTML = message;
-    messageContainer.append(messageDiv)
+
+function userJoined(name) {
+  const joined = document.createElement("div");
+  joined.classList.add("joined");
+  joined.innerHTML = name;
+  messageContainer.append(joined);
+}
+
+function appendMessage(message, e) {
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+  messageDiv.innerHTML = message;
+  messageContainer.append(messageDiv);
 }
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const io = require("socket.io")(3000);
+const users={}
 io.on("connection", (socket) => {
   console.log("NEW USET");
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    socket.broadcast.emit('user-connected',name)
+  })
   socket.emit("chat-message", "Hello");
-  socket.on("send-chat-message", (message) => {
+    socket.on("send-chat-message", (message) => {
+      console.log(message);
     socket.broadcast.emit('chat-message',message)
   });
 });
